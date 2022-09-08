21befff75b2695355f085ad4de189bff818d8ceb
Fernando Cunha <cunhafernandofj@gmail.com>
2022-09-08 01:07:09 -0300

    javascript

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,10 +64,13 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  old_verbose = $VERBOSE
+  $VERBOSE = nil
   Date::MONTHNAMES = [nil] + %w(Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)  
   Date::DAYNAMES = %w(Domingo Segunda-Feira Terça-Feira Quart-Feira Quinta-Feira Sexta-Feira Sábado)  
   Date::ABBR_MONTHNAMES = [nil] + %w(Jan Fev Mar Abr Mai Jun Jul Aug Sep Out Nov Dez)  
   Date::ABBR_DAYNAMES = %w(Dom Seg Ter Qua Qui Sex Sab)
+  $VERBOSE = old_verbose
   
   class Time  
     alias :strftime_nolocale :strftime  
